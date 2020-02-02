44806859ac7505eddc333e57aff6e032e1be1ffb
ishadif <ishadi.fauzan@hotmail.com>
2020-02-03 03:54:18 +0700

    Updates

---
+++ precache-manifest.0ee29725eae3c91249a900cc7476bafe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74c7d4d188048ff4b92f9830e5056fe",
+    "url": "/fabelio-test-ishadi/index.html"
+  },
+  {
+    "revision": "5bf2d180f4b6bf4b9598",
+    "url": "/fabelio-test-ishadi/static/css/main.d2b59fa5.chunk.css"
+  },
+  {
+    "revision": "b39f584b8a6048058ca5",
+    "url": "/fabelio-test-ishadi/static/js/2.875f5489.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fabelio-test-ishadi/static/js/2.875f5489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bf2d180f4b6bf4b9598",
+    "url": "/fabelio-test-ishadi/static/js/main.93c83491.chunk.js"
+  },
+  {
+    "revision": "fd0b5dc1a1100287dbdc",
+    "url": "/fabelio-test-ishadi/static/js/runtime-main.e776dc83.js"
+  }
+]);
